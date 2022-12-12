d22bce71e535f68e014bc8ebcccac7aa1fb5f2da
Kailar0000 <kailar0000@gmail.com>
2022-12-12 17:16:48 +0300

    Task19

---
+++ task19/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Clear();
+Console.Write("Введите число: ");
+string? number = Console.ReadLine();
+int i = 0;
+int N = number.Length;
+int n = N;
+bool x = true;
+
+while( i < n && x == true)
+{
+    if(number[i] != number[n])
+    {
+        x = false;
+    }
+    i ++;
+    n = N - i;
+}
+if(x == true)
+    Console.WriteLine("Да");
+else
+    Console.WriteLine("Нет");
